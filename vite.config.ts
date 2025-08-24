8d6434a031b12d15b9b1505230fb63cb6d0bb21b
ericshalbee <ericshalbee@gmail.com>
2025-08-24 16:55:48 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -16,15 +15,9 @@ export default defineConfig({
     },
     cssCodeSplit: true,
     sourcemap: false,
-    // Ensure public files are copied
     copyPublicDir: true
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  server: {
-    headers: {
-      'Cache-Control': 'public, max-age=31536000'
-    }
   }
 });
